f6e9eceb872d2b1843a9d6f007ee06a92226c9e0
Jonathan Hartman <j@p4nt5.com>
2015-04-24 12:31:00 -0700

    Initial commit; freshly generated

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+Divvy Cookbook CHANGELOG
+========================
+
+v0.0.1 (2015-04-24)
+-------------------
+- Development started
---
+++ metadata.rb
@@ -0,0 +1,11 @@
+# Encoding: UTF-8
+#
+# rubocop:disable SingleSpaceBeforeFirstArg
+name             'divvy'
+maintainer       'Jonathan Hartman'
+maintainer_email 'j@p4nt5.com'
+license          'apache2'
+description      'Installs/Configures divvy'
+long_description 'Installs/Configures divvy'
+version          '0.0.1'
+# rubocop:enable SingleSpaceBeforeFirstArg
