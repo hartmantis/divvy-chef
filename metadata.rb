3bd24470cc1f104c603d84c2899e7432d2820865
Jonathan Hartman <j@p4nt5.com>
2015-05-06 14:22:16 -0700

    Bump cookbook dep pins

--- metadata.rb
+++ metadata.rb
@@ -9,8 +9,8 @@ description      'Installs/Configures Divvy'
 long_description 'Installs/Configures Divvy'
 version          '0.1.1'
 
-depends          'mac-app-store', '~> 0.1'
-depends          'macosx_accessibility', '~> 0.1'
+depends          'mac-app-store', '~> 1.0'
+depends          'macosx_accessibility', '~> 1.0'
 depends          'windows', '~> 1.36'
 
 supports         'mac_os_x'
